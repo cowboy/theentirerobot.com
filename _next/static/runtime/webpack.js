decaa27784faabbdd16529e22404bc84b26723f9
Ben Alman <cowboy@rj3.net>
2018-12-24 12:44:46 -0500

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "91f1ff6d2da01bd64942";
+/******/ 	var hotCurrentHash = "814ad93a202e622aa7d5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
