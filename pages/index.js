5281071e28c5f0cc3a84125ec30971c30a6d906f
Ben Alman <cowboy@rj3.net>
2018-12-24 12:16:03 -0500

    Tweak layout, etc

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,43 @@
+import Head from 'next/head'
+
 const Index = () => (
   <div>
+    <Head>
+      <title>The Entire Robot</title>
+      <meta charSet='utf-8' />
+      <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    </Head>
     <div id="content-bg">
       <div id="content">
         <header>
           <h1>The Entire Robot</h1>
-          <h2>The music of “Cowboy” Ben Alman.</h2>
+          <h2>Electronic Funk Jazz Whatever by “Cowboy” Ben Alman</h2>
         </header>
         <footer>
           <p>
-            <a href="https://www.youtube.com/channel/UCFaB1HnL2RgFgD6v9MSgv2Q">Subscribe on YouTube</a>
-            {' | '}
-            <a href="https://www.facebook.com/TheEntireRobot/">Like on Facebook</a>
+            <a className='symbol' title='circleyoutube' href="https://www.youtube.com/channel/UCFaB1HnL2RgFgD6v9MSgv2Q">
+              Videos
+            </a>
+            <a className='symbol' title='circlefacebook' href="https://www.facebook.com/TheEntireRobot/">
+              Updates
+            </a>
           </p>
         </footer>
       </div>
     </div>
     <style jsx global>{`
-      @import url('https://fonts.googleapis.com/css?family=Bungee+Outline|Roboto|Open+Sans');
+      @import url('https://fonts.googleapis.com/css?family=Bungee+Outline');
+      @import url('/static/monosocialiconsfont/monosocialiconsfont.css');
       body {
         margin: 0;
         padding: 0;
         height: 100vh;
+        background-color: #000;
         background-image: url('/static/wires-min.jpg');
         background-size: cover;
         background-repeat: no-repeat;
         background-position: center;
-        font-family: 'Roboto', sans-serif;
-        font-family: 'Open Sans', sans-serif;
+        font-family: 'Mono Social Icons Font';
       }
     `}</style>
     <style jsx>{`
@@ -35,54 +46,48 @@ const Index = () => (
         left: 0;
         bottom: 0;
         right: 0;
-        background-image: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,1));
-        text-shadow: 4px 4px 8px #000;
+        background-image: linear-gradient(to bottom, rgba(0,0,0,0), rgba(0,0,0,0.8));
+        text-shadow: 4px 4px 10px #000;
         color: #fff;
+        padding-top: 5em;
       }
 
       #content {
-        padding: 1.5rem;
+        padding: 1.5em;
       }
 
       header {
         color: #68d4c0;
       }
 
-      header > :first-child {
-        margin-top: 4rem;
-      }
-
       h1 {
         font-family: 'Bungee Outline', cursive;
         font-weight: 700;
-        font-size: 4rem;
-        margin-bottom: 0;
+        font-size: 4em;
+        margin: 0;
+        margin-left: -3px;
+        position: relative;
       }
 
       h2 {
-        font-size: 1rem;
-      }
-
-      footer {
-        text-align: right;
-        margin-top: 4rem;
-      }
-
-      footer > :last-child {
-        margin-bottom: 0;
+        font-size: 0.9em;
+        margin: 0.7em 0 5em;
       }
 
       p {
-        font-size: 1rem;
+        font-size: 1.4em;
+        margin: 0;
       }
 
       a {
-        text-decoration: none;
-        color: #ffaa00;
+        text-shadow: none;
+        color: #000;
+        background-color: #68d4c0;
+        margin-right: 0.5em;
       }
 
       a:hover {
-        opacity: 0.6;
+        background-color: #fff;
       }
     `}</style>
   </div>
