e4b915130a78de3ef42fbba2f94350aaa791fbee
Ben Alman <cowboy@rj3.net>
2018-12-26 23:23:16 -0500

    Use CSS Modules, refactor CSS, randomize text color

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-module.exports = {
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+  cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  },
+  plugins: {
+    'postcss-css-variables': {}
+  },
   exportPathMap: function () {
     return {
       '/': { page: '/' }
     }
   }
-}
+})
