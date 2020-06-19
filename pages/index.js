4c6b3634fd6fc75e85b82f3df1719ff9a1c96bcf
Ben Alman <cowboy@rj3.net>
2020-06-19 17:32:28 -0400

    Social updates and many tweaks

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import classnames from 'classnames'
 
 import styles from './index.css'
 
@@ -18,9 +17,11 @@ const color = colors[Math.floor(Math.random() * colors.length)]
 const Index = () => (
   <>
     <Head>
-      <title>The Entire Robot</title>
+      <title>The Entire Robot :: Electronic Funk Jazz Whatever</title>
       <meta charSet='utf-8' />
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      <link rel="icon" href="/static/favicon.png"></link>
+      <script src='https://kit.fontawesome.com/6e08304a81.js' crossorigin='anonymous'></script>
       <style>{`
         :root {
           --primaryColor: ${color} !important;
@@ -35,29 +36,35 @@ const Index = () => (
         </h2>
       </header>
       <footer className={styles.footer}>
-        <ul className={styles.footerList}>
-          <li className={styles.footerItem}>
-            <a className={classnames('symbol', styles.link)}
-              title='circleyoutube'
-              href='https://www.youtube.com/channel/UCFaB1HnL2RgFgD6v9MSgv2Q'
-            >
-              Subscribe
+        <ul>
+          <li>
+            <a href='https://www.youtube.com/channel/UCFaB1HnL2RgFgD6v9MSgv2Q'>
+              <i className='fab fa-youtube' />
+              YouTube
             </a>
           </li>
-          <li className={styles.footerItem}>
-            <a className={classnames('symbol', styles.link)}
-              title='circlefacebook'
-              href='https://www.facebook.com/TheEntireRobot/'
-            >
-              Like
+          <li>
+            <a href='https://www.twitch.tv/theentirerobot'>
+              <i className='fab fa-twitch' />
+              Twitch
             </a>
           </li>
-          <li className={styles.footerItem}>
-            <a className={classnames('symbol', styles.link)}
-              title='circletwitter'
-              href='https://twitter.com/TheEntireRobot'
-            >
-              Follow
+          <li>
+            <a href='https://www.facebook.com/TheEntireRobot/'>
+              <i className='fab fa-facebook' />
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a href='https://twitter.com/TheEntireRobot'>
+              <i className='fab fa-twitter' />
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a href='https://www.instagram.com/theentirerobot/'>
+              <i className='fab fa-instagram' />
+              Instagram
             </a>
           </li>
         </ul>
