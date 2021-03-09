610371266b13fd4583fef00edafefb455c062d58
cowboy <cowboy@users.noreply.github.com>
2021-03-09 04:50:46 +0000

    deploy: 38218d440711a887cf94f094b64ecb1711e80290

---
+++ _next/static/OBS8K2eNqNw2snx0ROIPZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Ff8cd63fcaa9652992304.css","static\u002Fchunks\u002Fpages\u002Findex-2d8322df4ce9d554e2ae.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-762cca95af5378482016.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/styles.070d71f382e1d6940a07.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{29:function(e,t,_){e.exports={content:"content___3tKqh",header:"header___3WU5y",title:"title___JXFef",subtitle:"subtitle___1pe-O"}}}]);
